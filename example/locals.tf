0d84ae50b2e92b58f50d1af717afea951c159a96
Arunsai14 <arun.sai@sourcefuse.com>
2025-01-29 21:38:56 +0530

    initial commit

--- example/locals.tf
+++ example/locals.tf
@@ -24,10 +24,10 @@ locals {
 
     subnet_mapping = [
       {
-        subnet_id = data.aws_subnet.private.id
+        subnet_id = data.aws_subnet.private[each.key].id
       },
       {
-        subnet_id = data.aws_subnet.private.id
+        subnet_id = data.aws_subnet.private[each.key].id
       }
     ]
 
